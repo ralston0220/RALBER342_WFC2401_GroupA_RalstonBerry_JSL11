90b6ef530e1d1444629141d10a31b85ebeac23ad
ralston0220 <b8047585@gmail.com>
2024-06-21 10:45:59 +0200

    correct event listener in displayBoards function

--- index.js
+++ index.js
@@ -57,13 +57,13 @@ function displayBoards(boards) {
     const boardElement = document.createElement("button");
     boardElement.textContent = board;
     boardElement.classList.add("board-btn");
-    boardElement.addEventListener.click() => {
+    boardElement.addEventListener('click' () => {
       elements.headerBoardName.textContent = board;
       filterAndDisplayTasksByBoard(board);
-      activeBoard = board //assigns active board
-      localStorage.setItem("activeBoard", JSON.stringify(activeBoard))
-      styleActiveBoard(activeBoard)
-    };
+      activeBoard = board; //assigns active board
+      localStorage.setItem("activeBoard", JSON.stringify(activeBoard));
+      styleActiveBoard(activeBoard);
+    });
     boardsContainer.appendChild(boardElement);
   });
 
